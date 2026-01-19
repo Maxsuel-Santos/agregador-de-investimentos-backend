1956c27b585fb424d81d939214ef8e3f3d67d654
Maxsuel Santos <maxsuel.ap.l.santos@gmail.com>
2026-01-19 12:57:42 -0300

    Mapping the relationship between User and Account

---
+++ src/main/java/github/maxsuel/agregadordeinvestimentos/entity/Account.java
@@ -0,0 +1,38 @@
+package github.maxsuel.agregadordeinvestimentos.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "accounts")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Account {
+    
+    @Id
+    @Column(name = "account_id", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID accountId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+}   
